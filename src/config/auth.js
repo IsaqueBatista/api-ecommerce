d54d6a1825bd34e416b96f0da82777c377547f9e
isaquebatista <isaquebatista1716@gmail.com>
2022-10-20 13:43:17 -0300

    Configuration token JWT

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'e6df1c041ff97aae7af257dad26ce3d7',
+  expiresIn: '5d'
+}
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,8 @@ import SessionController from './app/controllers/SessionController'
 
 import UserController from './app/controllers/UserController'
 
+import authMiddleware from './app/middlewares/auth'
+
 const upload = multer(multerConfig)
 
 const routes = new Router()
@@ -14,6 +16,8 @@ const routes = new Router()
 routes.post('/users', UserController.store)
 
 routes.post('/sessions', SessionController.store)
+
+routes.use(authMiddleware) // Será chamado por todas as rotas ABAIXO
 
 routes.post('/products', upload.single('file'), ProductController.store)
 
