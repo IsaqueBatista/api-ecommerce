a63cff8883197c663bc82bf6e15f5c876cdd7822
isaquebatista <isaquebatista1716@gmail.com>
2022-12-10 12:16:17 -0300

    Correção Scheme de pedidos

--- src/app/schemas/Order.js
+++ src/app/schemas/Order.js
@@ -1,52 +1,55 @@
 import mongoose from 'mongoose'
 
-const OrderSchema = new mongoose.Schema({
+const OrderSchema = new mongoose.Schema(
 
-  user: {
-    type: String,
-    required: true
-  },
-  name: {
-    type: String,
-    required: true
-  },
-
-  products: [
-    {
+  {
+    user: {
       id: {
-        type: Number,
+        type: String,
         required: true
       },
       name: {
         type: String,
         required: true
-      },
-      price: {
-        type: Number,
-        required: true
-      },
-      category: {
-        type: String,
-        required: true
-      },
-      url: {
-        type: String,
-        required: true
-      },
-      quantity: {
-        type: Number,
-        required: true
       }
+    },
+
+    products: [
+      {
+        id: {
+          type: Number,
+          required: true
+        },
+        name: {
+          type: String,
+          required: true
+        },
+        price: {
+          type: Number,
+          required: true
+        },
+        category: {
+          type: String,
+          required: true
+        },
+        url: {
+          type: String,
+          required: true
+        },
+        quantity: {
+          type: Number,
+          required: true
+        }
+      }
+    ],
+    status: {
+      type: String,
+      required: true
     }
-  ],
-  status: {
-    type: String,
-    required: true
+  },
+  {
+    timestamps: true
   }
-},
-{
-  timestamps: true
-}
 )
 
 export default mongoose.model('Order', OrderSchema)
