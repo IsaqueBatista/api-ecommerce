5e3848034d27740a67cd90dddcd2f7cd1f9bc031
isaquebatista <isaquebatista1716@gmail.com>
2022-12-13 10:57:50 -0300

    Update do Produto

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -6,9 +6,10 @@ import User from '../models/User'
 class ProductController {
   async store (request, response) {
     const schema = Yup.object().shape({
-      name: Yup.string().required(),
-      price: Yup.number().required(),
-      category_id: Yup.number().required()
+      name: Yup.string(),
+      price: Yup.number(),
+      category_id: Yup.number(),
+      offer: Yup.boolean()
     })
 
     try {
@@ -24,13 +25,14 @@ class ProductController {
     }
 
     const { filename: path } = request.file
-    const { name, price, category_id } = request.body
+    const { name, price, category_id, offer } = request.body
 
     const product = await Product.create({
       name,
       price,
       category_id,
-      path
+      path,
+      offer
     })
 
     return response.json(product)
@@ -48,6 +50,54 @@ class ProductController {
     })
 
     return response.json(products)
+  }
+
+  async update (request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      price: Yup.number(),
+      category_id: Yup.number(),
+      offer: Yup.boolean()
+    })
+
+    try {
+      await schema.validateSync(request.body, { abortEarly: false })
+    } catch (err) {
+      return response.status(400).json({ error: err.errors })
+    }
+
+    const { admin: isAdmin } = await User.findByPk(request.userId)
+
+    if (!isAdmin) {
+      return response.status(401).json()
+    }
+
+    const { id } = request.params
+
+    const product = await Product.findByPk(id)
+
+    if (!product) {
+      return response.status(401).json({ error: 'Make sure your product ID is correct' })
+    }
+
+    let path
+    if (request.file) {
+      path = request.file.filename
+    }
+
+    const { name, price, category_id, offer } = request.body
+
+    await Product.update({
+      name,
+      price,
+      category_id,
+      path,
+      offer
+    },
+    { where: { id } }
+    )
+
+    return response.status(200).json()
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -22,6 +22,7 @@ routes.use(authMiddleware) // Será chamado por todas as rotas ABAIXO
 
 routes.post('/products', upload.single('file'), ProductController.store)
 routes.get('/products', ProductController.index)
+routes.put('/products/:id', upload.single('file'), ProductController.update)
 
 routes.post('/categories', CategoryController.store)
 routes.get('/categories', CategoryController.index)
