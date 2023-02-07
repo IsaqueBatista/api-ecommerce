dfe4c4d9c0eac166f8c27b60d903ed5218fdde86
isaquebatista <isaquebatista1716@gmail.com>
2023-02-07 16:20:32 -0300

    Alterando status para 409

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -34,7 +34,7 @@ class UserController {
     })
 
     if (userExists) {
-      return response.status(400).json({ error: 'User already exists' })
+      return response.status(409).json({ error: 'User already exists' })
     }
 
     const user = await User.create({
